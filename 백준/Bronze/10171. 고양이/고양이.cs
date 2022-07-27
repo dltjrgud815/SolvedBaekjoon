cea6b36985db7a3e0c44f6bc635e6b255a6ec0a5
dltjrgud815 <60725734+dltjrgud815@users.noreply.github.com>
2022-07-27 19:54:58 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 5304 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Text;
+
+class SolvedBaekjoon
+{
+    static void Main()
+    {
+        Console.WriteLine(@"\    /\");
+        Console.WriteLine(" )  ( ')");
+        Console.WriteLine("(  /  )");
+        Console.WriteLine(@" \(__)|");
+    }
+}
