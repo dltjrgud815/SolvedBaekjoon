30e825559a270e9cb00cc9d81978973a2dfeca8e
SeokHyoung Lee <60725734+dltjrgud815@users.noreply.github.com>
2023-07-08 18:25:56 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 60 ms, Memory: 5376 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string input = System.Console.ReadLine();
+        System.Console.WriteLine($"{input}\n1");
+    }
+}
